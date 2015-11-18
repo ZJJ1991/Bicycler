0193d873d7d355b4dc6a37366190323fa088e55d
ZJJ1991 <zhoujunjie.10@gmail.com>
2015-11-18 18:39:39 +0200

    close to perfect

--- app/src/main/java/com/example/jzhou/bicycler/Plugin.java
+++ app/src/main/java/com/example/jzhou/bicycler/Plugin.java
@@ -1,6 +1,7 @@
 package com.example.jzhou.bicycler;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -18,6 +19,9 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +35,8 @@ import com.aware.providers.Magnetometer_Provider;
 
 import java.util.UUID;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 public class Plugin extends AppCompatActivity {
 
     private static String DEBUG = "DEBUGGING";
@@ -42,11 +48,15 @@ public class Plugin extends AppCompatActivity {
     public static double accelerometer_y;
     public static double accelerometer_z;
     public static double acc;
-    public TextView txtview;
-    public TextView acc_txt;
+
     public PostgreSqlCon Posconn;
     public static LongOperation lo;
     public static char[] Device_id;
+    public static RadioGroup gpsradioGroup;
+    public static RadioGroup accradioGroup;
+    public static Button start_btn;
+    public static Button stop_btn;
+
 
 
     @Override
@@ -65,37 +75,127 @@ public class Plugin extends AppCompatActivity {
             }
         });
 
-        txtview = (TextView) findViewById(R.id.speed);
-        acc_txt = (TextView) findViewById(R.id.accelerometer);
+
+
+
+
         String device_id = Aware_Preferences.DEVICE_ID;
         Device_id = device_id.toCharArray();
         Log.d("device_id", Aware_Preferences.DEVICE_ID);
+
+
+
+        //initialise Aware
         Intent aware = new Intent(this, Aware.class);
         startService(aware);
 
+        //Activate sensor
+
+
+
+
         Log.d(DEBUG, "1");
 
-        Aware.setSetting(this, Aware_Preferences.STATUS_LOCATION_GPS, true);
-        Log.d(DEBUG, "3");
-        Aware.setSetting(this, Aware_Preferences.FREQUENCY_LOCATION_GPS, 10);
+        start_btn = (Button) findViewById(R.id.start_btn);
+        stop_btn = (Button) findViewById(R.id.stop_btn);
 
-        Aware.setSetting(this, Aware_Preferences.STATUS_ACCELEROMETER, true);
-        Aware.setSetting(this, Aware_Preferences.FREQUENCY_ACCELEROMETER, 200000);
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Aware.setSetting(Plugin.this, Aware_Preferences.STATUS_LOCATION_GPS, true);
+                Log.d(DEBUG, "3");
+                Aware.setSetting(Plugin.this, Aware_Preferences.FREQUENCY_LOCATION_GPS, 10);
 
 
+                Aware.setSetting(Plugin.this, Aware_Preferences.STATUS_ACCELEROMETER, true);
+                Aware.setSetting(Plugin.this, Aware_Preferences.FREQUENCY_ACCELEROMETER, 200000);
+
+                Aware.startSensor(Plugin.this, Aware_Preferences.STATUS_ACCELEROMETER);
+                Aware.startSensor(Plugin.this, Aware_Preferences.STATUS_LOCATION_GPS);
+            }
+        });
+
+        stop_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDestroy();
+            }
+        });
+
+        gpsradioGroup = (RadioGroup) findViewById(R.id.Gps_fre_radio);
+        gpsradioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int radioButtonID = group.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(radioButtonID);
+                if (rb.getText().equals("180 seconds")) {
+                    Log.d(DEBUG, "set gps frequency 180");
+                    Aware.setSetting(Plugin.this, Aware_Preferences.FREQUENCY_LOCATION_GPS, 180);
+                    Log.d(DEBUG, "finished gps frequency 180");
+                    Toast toast = Toast.makeText(Plugin.this, "GPS frequency has been changed", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                if (rb.getText().equals("30 seconds")) {
+                    Log.d(DEBUG, "set gps frequency 30");
+                    Aware.setSetting(Plugin.this, Aware_Preferences.FREQUENCY_LOCATION_GPS, 30);
+                    Log.d(DEBUG, "finished gps frequency 30");
+                    Toast toast = Toast.makeText(Plugin.this, "GPS frequency has been changed", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                if (rb.getText().equals("10 seconds")) {
+                    Log.d(DEBUG, "set gps frequency 10");
+                    Aware.setSetting(Plugin.this, Aware_Preferences.FREQUENCY_LOCATION_GPS, 10);
+                    Log.d(DEBUG, "finished gps frequency 10");
+                    Toast toast = Toast.makeText(Plugin.this, "GPS frequency has been changed", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+
+
+        accradioGroup = (RadioGroup) findViewById(R.id.Acc_fre_radio);
+        accradioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                int radioButtonID = group.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(radioButtonID);
+                if (rb.getText().equals("200000 (normal)")) {
+                    Log.d(DEBUG, "frequency 200000");
+                    Aware.setSetting(Plugin.this, Aware_Preferences.FREQUENCY_ACCELEROMETER, 200000);
+                    Log.d(DEBUG, "finished frequency 200000");
+                    Toast toast = Toast.makeText(Plugin.this, "Accelerometer frequency has been changed", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                if (rb.getText().equals("60000 (UI)")){
+                    Log.d(DEBUG,"frequency 60000");
+                    Aware.setSetting(Plugin.this, Aware_Preferences.FREQUENCY_ACCELEROMETER, 60000);
+                    Log.d(DEBUG, "finished frequency 60000");
+                    Toast toast = Toast.makeText(Plugin.this, "Accelerometer frequency has been changed", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                if (rb.getText().equals("20000 (game)")){
+                    Log.d(DEBUG,"frequency 20000");
+                    Aware.setSetting(Plugin.this, Aware_Preferences.FREQUENCY_LOCATION_GPS, 20000);
+                    Log.d(DEBUG, "finished frequency 20000");
+                    Toast toast = Toast.makeText(Plugin.this, "Accelerometer frequency has been changed", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                if (rb.getText().equals("0 (fastest)")){
+                    Log.d(DEBUG,"frequency 0");
+                    Aware.setSetting(Plugin.this, Aware_Preferences.FREQUENCY_ACCELEROMETER, 0);
+                    Log.d(DEBUG, "finished frequency 0");
+                    Toast toast = Toast.makeText(Plugin.this, "Accelerometer frequency has been changed", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
 
         IntentFilter filter = new IntentFilter();
         Log.d(DEBUG, "4");
 
         filter.addAction(Locations.ACTION_AWARE_LOCATIONS);
         filter.addAction(Accelerometer.ACTION_AWARE_ACCELEROMETER);
-
-        Intent intent = new Intent(Locations.ACTION_AWARE_LOCATIONS);
-        sendBroadcast(intent);
-
-        Intent acc_intent= new Intent(Accelerometer.ACTION_AWARE_ACCELEROMETER);
-        sendBroadcast(acc_intent);
-
         registerReceiver(Datareceiver, filter);
         Log.d(DEBUG, "5");
 
@@ -128,7 +228,7 @@ public class Plugin extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals(Locations.ACTION_AWARE_LOCATIONS)){
-                Log.d(DEBUG,"location data ");
+                Log.d(DEBUG,"try getting location data ");
                 Cursor location = context.getContentResolver().query
                         (Locations_Provider.Locations_Data.CONTENT_URI, null, null, null, Locations_Provider.Locations_Data.TIMESTAMP + " DESC LIMIT 1");
                 Log.d(DEBUG,"7");
@@ -139,7 +239,6 @@ public class Plugin extends AppCompatActivity {
                     longitude = location.getDouble(location.getColumnIndex(Locations_Provider.Locations_Data.LONGITUDE));
                     latitude = location.getDouble(location.getColumnIndex(Locations_Provider.Locations_Data.LATITUDE));
                     altitude = location.getDouble(location.getColumnIndex(Locations_Provider.Locations_Data.ALTITUDE));
-                    txtview.setText(longitude + " ");
                     long epoch = System.currentTimeMillis();
                     String timestamp = String.valueOf(epoch);
                     double Timestamp = Double.parseDouble(timestamp);
@@ -158,31 +257,49 @@ public class Plugin extends AppCompatActivity {
             }
 
             if (intent.getAction().equals(Accelerometer.ACTION_AWARE_ACCELEROMETER)){
-                Log.d(DEBUG,"try to get accelerometer data");
-                Cursor accelerometer  = context.getContentResolver().query
-                        (Accelerometer_Provider.Accelerometer_Data.CONTENT_URI, null, null, null, Accelerometer_Provider.Accelerometer_Data.TIMESTAMP + " DESC LIMIT 1");
-                Log.d(DEBUG, "data sql 1");
+                Log.d(DEBUG, "try to get accelerometer data");
 
-                Log.d(DEBUG, "data sql 2");
-                if (accelerometer!=null&&accelerometer.moveToFirst()){
-                    accelerometer_x = accelerometer.getDouble(accelerometer.getColumnIndex(Accelerometer_Provider.Accelerometer_Data.VALUES_0));
-                    accelerometer_y = accelerometer.getDouble(accelerometer.getColumnIndex(Accelerometer_Provider.Accelerometer_Data.VALUES_1));
-                    accelerometer_z = accelerometer.getDouble(accelerometer.getColumnIndex(Accelerometer_Provider.Accelerometer_Data.VALUES_2));
-                    Log.d(DEBUG, "value of acc");
-                    acc = Math.sqrt(accelerometer_x * accelerometer_x + accelerometer_y * accelerometer_y + accelerometer_z * accelerometer_z);
-                    acc_txt.setText(acc+" ");
-                    String device_id = "sadas-1wa";
-                    long epoch = System.currentTimeMillis();
-                    String timestamp = String.valueOf(epoch);
-                    double Timestamp = Double.parseDouble(timestamp);
+                ContentValues data = intent.getParcelableExtra(Accelerometer.EXTRA_DATA);
 
-                    String sql = "insert into bicyclers.\"Accelerometer\"(timestamp, value_x, value_y, value_z, value)values("+Timestamp+","+accelerometer_x+","+accelerometer_y+","+accelerometer_z+","+acc+")";
-                    Log.d(DEBUG, "data sql acce 1");
-                    Log.d(DEBUG, "data sql acce 2");
-                    Log.d("Device-id", Aware_Preferences.DEVICE_ID);
-                    lo = new LongOperation(sql);
-                    lo.execute();
-                }
+                accelerometer_x = data.getAsDouble(Accelerometer_Provider.Accelerometer_Data.VALUES_0);
+                accelerometer_y = data.getAsDouble(Accelerometer_Provider.Accelerometer_Data.VALUES_1);
+                accelerometer_z = data.getAsDouble(Accelerometer_Provider.Accelerometer_Data.VALUES_2);
+                Log.d(DEBUG, data.toString());
+                long epoch = System.currentTimeMillis();
+                String timestamp = String.valueOf(epoch);
+                double Timestamp = Double.parseDouble(timestamp);
+                String sql = "insert into bicyclers.\"Accelerometer\"(timestamp, value_x, value_y, value_z, value)values(" + Timestamp + "," + accelerometer_x + "," + accelerometer_y + "," + accelerometer_z + "," + acc  + ")";
+                lo = new LongOperation(sql);
+                lo.execute();
+                Log.d(DEBUG, "data sql acce 2");
+
+//                Cursor accelerometer  = context.getContentResolver().query
+//                        (Accelerometer_Provider.Accelerometer_Data.CONTENT_URI, null, null, null, Accelerometer_Provider.Accelerometer_Data.TIMESTAMP + " DESC LIMIT 1");
+//                Log.d(DEBUG, "cursor data");
+//                Log.d("test", accelerometer.moveToFirst()+"sa");
+//
+//                if (accelerometer!=null&&accelerometer.moveToFirst()) {
+//                    accelerometer_x = accelerometer.getDouble(accelerometer.getColumnIndex(Accelerometer_Provider.Accelerometer_Data.VALUES_0));
+//                    accelerometer_y = accelerometer.getDouble(accelerometer.getColumnIndex(Accelerometer_Provider.Accelerometer_Data.VALUES_1));
+//                    accelerometer_z = accelerometer.getDouble(accelerometer.getColumnIndex(Accelerometer_Provider.Accelerometer_Data.VALUES_2));
+//                    Log.d(DEBUG, "value of acc");
+//                    if (accelerometer != null && !accelerometer.isClosed()){accelerometer.close();}
+//                    accelerometer.close();
+//                    Log.d(DEBUG,"close cursor");
+//                    acc = Math.sqrt(accelerometer_x * accelerometer_x + accelerometer_y * accelerometer_y + accelerometer_z * accelerometer_z);
+//                    acc_txt.setText(acc + " ");
+//                    long epoch = System.currentTimeMillis();
+//                    String timestamp = String.valueOf(epoch);
+//                    double Timestamp = Double.parseDouble(timestamp);
+//
+//                    String sql = "insert into bicyclers.\"Accelerometer\"(timestamp, value_x, value_y, value_z, value, Device_id)values(" + Timestamp + "," + accelerometer_x + "," + accelerometer_y + "," + accelerometer_z + "," + acc + "sada" + ")";
+//                    Log.d(DEBUG, "data sql acce 1");
+//                    Log.d("Device-id", Aware_Preferences.DEVICE_ID);
+//                    lo = new LongOperation(sql);
+//                    lo.execute();
+//                    Log.d(DEBUG, "data sql acce 2");
+//                 }
+
             }
 
 
@@ -218,6 +335,7 @@ public class Plugin extends AppCompatActivity {
         Posconn.close();
 
         unregisterReceiver(Datareceiver);
+        Aware.setSetting(this, Aware_Preferences.STATUS_ACCELEROMETER, false);
         Aware.setSetting(this, Aware_Preferences.STATUS_LOCATION_GPS, false);
         Aware.stopPlugin(this,"com.example.jzhou.bicybler");
     }
